1995477fd8a2adec88ec1b0756461713b43b12b4
Victor Carmouze <victor.carmouze@blablacar.com>
2024-10-25 21:19:00 +0200

    [Refacto] Split Station details view into serval subviews

--- MyLittleRadioApp/MyLittleRadio/Features/StationDetailsFeature/StationDetailsView.swift
+++ MyLittleRadioApp/MyLittleRadio/Features/StationDetailsFeature/StationDetailsView.swift
@@ -17,42 +17,85 @@ struct StationDetailsView: View {
     var body: some View {
         WithPerceptionTracking {
             if let selectedStation = store.selectedStation {
-                let backgroundColor = selectedStation.colors.primary.toColor
-                let foregroundColor: Color = backgroundColor.isLight ? .black : .white
-                Rectangle()
-                    .fill(backgroundColor)
-                    .overlay(
-                        VStack {
-                            HStack {
-                                Text(selectedStation.title)
-                                    .font(.title)
-                                    .padding()
-                                    .multilineTextAlignment(.center)
-                                    .foregroundColor(foregroundColor)
-
-                                Spacer()
-
-                                Button(action: onDismiss) {
-                                    Image(systemName: "xmark")
-                                        .foregroundColor(foregroundColor)
-                                        .padding()
-                                }
-                            }
-                            .padding(.top, 16)
-                            Button(action: {
-                                store.send(.togglePlayPause)
-                            }, label: {
-                                Image(systemName: store.isPlaying ? "pause.circle.fill" : "play.circle.fill")
-                                    .resizable()
-                                    .frame(width: 60, height: 60)
-                                    .foregroundColor(foregroundColor)
-                                    .padding(.top, 20)
-                            })
-                            Spacer()
-                        } .padding()
-                    )
-                    .ignoresSafeArea()
+                StationBackgroundView(
+                    selectedStation: selectedStation,
+                    foregroundColor: selectedStation.colors.primary.toColor.isLight ? .black : .white,
+                    onDismiss: onDismiss,
+                    isPlaying: store.isPlaying,
+                    togglePlayPause: { store.send(.togglePlayPause) }
+                )
             }
+        }
+    }
+}
+
+struct StationBackgroundView: View {
+    let selectedStation: Station
+    let foregroundColor: Color
+    let onDismiss: () -> Void
+    let isPlaying: Bool
+    let togglePlayPause: () -> Void
+
+    var body: some View {
+        Rectangle()
+            .fill(selectedStation.colors.primary.toColor)
+            .overlay(
+                VStack {
+                    StationHeaderView(
+                        title: selectedStation.title,
+                        foregroundColor: foregroundColor,
+                        onDismiss: onDismiss
+                    )
+                    PlayPauseButton(
+                        isPlaying: isPlaying,
+                        foregroundColor: foregroundColor,
+                        togglePlayPause: togglePlayPause
+                    )
+                    Spacer()
+                }
+                .padding()
+            )
+            .ignoresSafeArea()
+    }
+}
+
+struct StationHeaderView: View {
+    let title: String
+    let foregroundColor: Color
+    let onDismiss: () -> Void
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.title)
+                .padding()
+                .multilineTextAlignment(.center)
+                .foregroundColor(foregroundColor)
+
+            Spacer()
+
+            Button(action: onDismiss) {
+                Image(systemName: "xmark")
+                    .foregroundColor(foregroundColor)
+                    .padding()
+            }
+        }
+        .padding(.top, 16)
+    }
+}
+
+struct PlayPauseButton: View {
+    let isPlaying: Bool
+    let foregroundColor: Color
+    let togglePlayPause: () -> Void
+
+    var body: some View {
+        Button(action: togglePlayPause) {
+            Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                .resizable()
+                .frame(width: 60, height: 60)
+                .foregroundColor(foregroundColor)
+                .padding(.top, 20)
         }
     }
 }
