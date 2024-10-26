cdd788467e53f0b74af83fe0f260e983cb32e087
Victor Carmouze <victor.carmouze@blablacar.com>
2024-10-26 17:34:28 +0200

    [Network] Reorganize network files, and adding proper error management

---
+++ MyLittleRadioApp/Modules/Networking/Sources/Networking/Configurations/StationEndpointConfiguration.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct StationsEndpointConfiguration {
+
+    static var stations: String { BaseEndpointConfiguration.baseURL + "/stations" }
+}
---
+++ MyLittleRadioApp/Modules/Networking/Sources/Networking/Internal/APIError.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+enum ApiError: LocalizedError {
+    case invalidURL
+    case decodingError
+    case networkError(URLError)
+    case unknown
+
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "The URL provided is invalid."
+        case .decodingError:
+            return "Failed to decode the response data."
+        case .networkError(let urlError):
+            return "Network error: \(urlError.localizedDescription)"
+        case .unknown:
+            return "An unknown error occurred."
+        }
+    }
+}
